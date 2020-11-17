b619f98277727aa819933ca0ad8b3e2b67d0a72b
hdiddle13 <bharry@mit.edu>
2020-11-17 00:00:50 -0500

    new current user approach

--- public/host.js
+++
@@ -1,9 +0,0 @@
-(function(){
-    const email    = document.getElementById('emailInput');
-    if (firebase.auth() != null) {
-        email.placeholder = firebase.auth().email;
-        email.value = firebase.auth().email;
-    } else {
-        email.placeholder = "Please Log In Before Posting";
-    }
-}());
